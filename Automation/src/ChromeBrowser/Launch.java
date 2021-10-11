833a27f4b77a8a07d160c5268b971a1907a4099c
Vencent <aqqq15219@gmail.com>
2021-10-11 11:59:55 +1100

    added

--- Automation/src/ChromeBrowser/Launch.java
+++ Automation/src/ChromeBrowser/Launch.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.github.javafaker.Faker;sdasds
 sadsda
-public class Launch {
+public class Launch {asdasd
 
 	public static void main(String[] args) throws InterruptedException{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\eclipse-workspace\\Automation\\lib\\chromedriver.exe");
